01245ae1637b8ea0922aa3fb9d4cfcc053fda8a5
米澤凛太郎 <yonezawarintarou@M4-YoneBook-Pro.local>
2025-05-01 13:47:13 +0900

    変更を反映

--- pre_register.js
+++ pre_register.js
@@ -1,38 +1,38 @@
 document.getElementById("registerForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
-  
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const question = document.getElementById("question").value.trim();
-    const message = document.getElementById("message");
-  
-    if (!name || !email || !question) {
-      message.textContent = "すべての項目を入力してください。";
-      return;
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const question = document.getElementById("question").value.trim();
+  const message = document.getElementById("message");
+
+  if (!name || !email || !question) {
+    message.textContent = "すべての項目を入力してください。";
+    return;
+  }
+
+  const scriptURL = "https://script.google.com/macros/s/AKfycbwohaRFmlvZevXv-PpOMerF4xhYHYPjOKsdTlW7cLvXdSqj1bCV7xTgWjxQNW1bryVc/exec";
+
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("question", question);
+
+  try {
+    console.log("送信開始");
+
+    const res = await fetch(scriptURL, { method: "POST", body: formData });
+    const text = await res.text();
+    console.log("GASからの応答:", text);
+
+    if (text === "OK") {
+      window.location.href = "waiting.html";
+    } else {
+      message.textContent = "GASが異常な応答を返しました：" + text;
     }
-  
-    const scriptURL = "https://script.google.com/macros/s/AKfycbxMceZfl1p4-phd-WaDcc6-RXPg3MzXDPe6IrJ86Ehauf8wJBkcz3rlFBH-h4k5hntY/exec";
-  
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("email", email);
-    formData.append("question", question);
-  
-    try {
-      console.log("送信開始");
 
-      const res = await fetch(scriptURL, { method: "POST", body: formData });
-      const text = await res.text();
-      console.log("GASからの応答:", text);
-
-      if (text === "OK") {
-        window.location.href = "waiting.html";
-      } else {
-        message.textContent = "GASが異常な応答を返しました：" + text;
-      }
-
-    } catch (error) {
-      console.error("送信エラー:", error);
-      message.textContent = "送信に失敗しました。もう一度お試しください。";
-    }
-  });
+  } catch (error) {
+    console.error("送信エラー:", error);
+    message.textContent = "送信に失敗しました。もう一度お試しください。";
+  }
+});
